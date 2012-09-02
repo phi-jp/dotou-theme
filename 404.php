6c2dda813a627be2ab15a6abbe7be6588d7a22ff
RosettaHalt <craft@craft-notes.com>
2012-09-03 00:52:01 +0900

    Add 404.php

---
+++ 404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <?php get_template_part('head'); ?>
+    </head>
+    
+    <body>
+        <?php get_header(); ?>
+        <div id="contents">
+            このページは存在しません。
+        </div>
+        <?php get_footer(); ?>
+        
+        <?php wp_footer(); ?>
+    </body>
+</html>
