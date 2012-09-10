b78199433371128898660db264a3da83dd2aad2b
RosettaHalt <rosetta@lugn-design.com>
2012-09-10 18:57:28 +0900

    Add mainloop.

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <?php get_template_part('head'); ?>
-    </head>
-    
-    <body>
-        <?php get_header(); ?>
-        <div id="contents">
+<head>
+    <?php get_template_part('head'); ?>
+</head>
+
+<body>
+<?php get_header(); ?>
+<div id="contents">
+    <?php
+    if(have_posts()): while(have_posts()):the_post();
+        $title = get_the_title();
+        $link = get_permalink();
+        $date = get_the_time('Y/m/d');
+    ?>
+    <section class="entry">
+        <header class="postHeader">
+            <h1 class="postTitle"><?php echo $title; ?></h1>
+        </header>
+        <div class="post">
+            <div class="postContent"><?php the_content(); ?></div>
         </div>
-        <?php get_footer(); ?>
-        
-        <?php wp_footer(); ?>
-    </body>
+    </section>
+    <?php endwhile; endif; ?>
+</div>
+<?php get_footer(); ?>
+
+<?php wp_footer(); ?>
+</body>
 </html>
