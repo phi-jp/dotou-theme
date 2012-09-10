eac39a4e24988778839a2900414d04777d0bf387
RosettaHalt <rosetta@lugn-design.com>
2012-09-10 21:37:32 +0900

    Add page list on footer

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
 <footer id="footer">
+    <nav class="footerNav">
+        <ul class="cf">
+            <?php wp_list_pages('title_li='); ?>
+        </ul>
+    </nav>
     <address>
         Copyright &copy; <?php bloginfo('name'); ?>. All Rights Reserved.
     </address>
