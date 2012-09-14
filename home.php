c318296a03b5b29a309a8463036b948c8c1a3476
RosettaHalt <rosetta@lugn-design.com>
2012-09-14 14:30:11 +0900

    Add home.php

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <?php get_template_part('head'); ?>
+</head>
+
+<body>
+<?php get_header(); ?>
+<div id="contents">
+    <div class="blanding">
+        <div class="slide">
+            <ul>
+                <li><a href="">slide01</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="contentList">
+        <ul>
+            <li><a href="">試練</a></li>
+            <li><a href="">修練</a></li>
+            <li><a href="">鍛錬</a></li>
+        </ul>
+    </div>
+</div>
+<?php get_footer(); ?>
+
+<?php wp_footer(); ?>
+</body>
+</html>
