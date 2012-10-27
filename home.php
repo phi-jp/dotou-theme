2b9d7e9b841d3b70a484b1aebf2fe33d5917ad86
RosettaHalt <halt.develop@gmail.com>
2012-10-28 00:13:22 +0900

    fix home

--- home.php
+++ home.php
@@ -8,39 +8,73 @@
 <body <?php body_class(); ?>>
 <?php get_header(); ?>
 <div id="contents">
-  <div class="blanding cf">
-    <section>
-      <h2>怒涛とは</h2>
-      <p>修練・鍛錬・熟練</p>
-      <p>コードを実際に組んで学んでいこう。</p>
-      <p>現在はn種類の言語をサポートしています。<br />今後も様々な言語を追加していく予定です。</p>
-      <a class="button" href="">Let's Start!</a>
-    </section>
-    <div class="image">
+  <div class="blanding">
+
+    <div class="main_blanding cf">
+      <section>
+        <h2>怒涛とは</h2>
+        <p>修練・鍛錬・熟練の3つの学習タイプ。<br>だみーーーー</p>
+        <dl>
+          <dt>修練</dt>
+          <dd>修練はサンプルのコードを載せています</dd>
+          <dt>鍛錬</dt>
+          <dd>jsdo.itで実際にコードを書いてみよう。<br />
+            うまく動かない場合は修練のサンプルで確認してみよう。</dd>
+          <dt>熟練</dt>
+          <dd>熟練では基礎を終えた人向けに一歩進んだ内容を紹介しています。</dd>
+          <dt>巻物</dt>
+          <dd>書籍紹介</dd>
+        </dl>
+      </section>
+      <div class="image">
+      </div>
     </div>
+<!--
+    <div class="sub_blanding cf">
+      <section>
+        <h2>修練でコードを見る</h2>
+        <p>
+          修練はサンプルのコードを載せています。
+        </p>
+      </section>
+
+      <section>
+        <h2>鍛錬でコードを書く</h2>
+        <p>
+          jsdo.itで実際にコードを書いてみよう。<br />
+          うまく動かない場合は修練のサンプルで確認してみよう。
+        </p>
+      </section>
+
+      <section>
+        <h2>熟練でレベルアップ</h2>
+        <p>
+          熟練では基礎を終えた人向けに一歩進んだ内容を紹介しています。<br />
+        </p>
+      </section>
+    </div>
+    -->
   </div>
 
-  <div class="about_list cf">
-    <section>
-      <h2>修練でコードを見る</h2>
-      <p>
-        修練はサンプルのコードを載せています。
-      </p>
+  <div class="dotou_content cf">
+    <section class="content_list">
+      <h2>怒涛の言語一覧</h2>
+      <ul class="dotou_lang cf">
+        <li><a href="">js</a></li>
+        <li><a href="">html</a></li>
+        <li><a href="">css</a></li>
+        <li><a href="">js</a></li>
+        <li><a href="">html</a></li>
+        <li><a href="">css</a></li>
+        <li><a href="">js</a></li>
+        <li><a href="">html</a></li>
+        <li><a href="">css</a></li>
+      </ul>
+      <p class="all_lang"><a href="">すべての言語はこちら</a></p>
     </section>
-
-    <section>
-      <h2>鍛錬でコードを書く</h2>
-      <p>
-        jsdo.itで実際にコードを書いてみよう。<br />
-        うまく動かない場合は修練のサンプルで確認してみよう。
-      </p>
-    </section>
-
-    <section>
-      <h2>熟練でレベルアップ</h2>
-      <p>
-        熟練では基礎を終えた人向けに一歩進んだ内容を紹介しています。<br />
-      </p>
+    <section class="news">
+      <p>news</p>
+      <p>sns</p>
     </section>
   </div>
 
