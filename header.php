e058f71159f934f7e3e98bd8ff07aa64084f5a36
RosettaHalt <halt.develop@gmail.com>
2012-11-03 21:38:03 +0900

    fix header

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
       <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?><!--HOME--></a></h1>
       <h2><?php bloginfo('description'); ?></h2>
     </hgroup>
-    <nav>
+    <nav class="global_nav">
       <ul class="cf">
         <li>
           <form method="get" action="<?php bloginfo('url'); ?>/">
@@ -12,7 +12,21 @@
             <input type="submit" value="検索">
           </form>
         </li>
-        <li><a href="">ログイン</a></li>
+        <li><a href="<?php echo home_url( '/' ); ?>wp-login.php">ログイン</a></li>
+      </ul>
+    </nav>
+  </div>
+  <div class="lang_nav">
+    <nav>
+      <ul>
+        <li class="list_title">言語一覧</li>
+        <?php
+        $allCategory = get_terms( "category", "fields=all&get=all" );
+        foreach($allCategory as $value):
+          if($value->parent == 0){ ?>
+            <li><a href="<?php echo get_category_link($value->term_id); ?>"><?php echo $value->name;?></a></li>
+            <?php }
+        endforeach; ?>
       </ul>
     </nav>
   </div>
