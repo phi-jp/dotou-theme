c615ebb2f5761dae4e28c30d1976164070f066af
RosettaHalt <halt.develop@gmail.com>
2012-11-09 21:30:21 +0900

    fix news

--- archive/news.php
+++ archive/news.php
@@ -1,6 +1,19 @@
 <div id="contents" class="cf">
   <h1 class="title"><?php wp_title(""); ?></h1>
   <div class="main_content cf">
-
+    <ul>
+      <?php /* main loop */
+      if(have_posts()): while(have_posts()):the_post();
+        $title = get_the_title();
+        $link = get_permalink();
+        $date = get_the_time('Y/m/d');
+        $excerpt = get_the_excerpt();
+        ?>
+      <li>
+        <h2><a href="<?php echo $link; ?>"><?php echo $title; ?></a></h2>
+        <p><?php echo $excerpt; ?></p>
+      </li>
+      <?php endwhile; endif; ?>
+    </ul>
   </div>
 </div>
--- side/news.php
+++ side/news.php
@@ -2,7 +2,7 @@
   <h2>最近のお知らせ</h2>
   <ul>
   <?php
-    $loop = new WP_Query( array( 'post_type' => 'news', 'posts_per_page' => 5, 'order' => 'ASC' ) );
+    $loop = new WP_Query( array( 'post_type' => 'news', 'posts_per_page' => 5, 'order' => 'DESC' ) );
     while ( $loop->have_posts() ) : $loop->the_post();
       $title = get_the_title();
       $link = get_permalink();
