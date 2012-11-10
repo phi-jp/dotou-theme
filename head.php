87e288bea133febe024b5094ebed86e9083bfb2b
RosettaHalt <halt.develop@gmail.com>
2012-11-11 04:55:29 +0900

    add google analytics

--- head.php
+++ head.php
@@ -30,6 +30,20 @@
 <!--<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/plugins/tmlib/tm.scroller.js"></script>-->
 <!-- tm scroller -->
 
+<!-- Google Analytics -->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36228975-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<!-- Google Analytics -->
+
 <!--[if IE]>
 <style>
   @import url("<?php bloginfo('template_url'); ?>/ie-style.css");
