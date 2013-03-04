665678c44f06cd86cc93f43106d05c9d2f15b5e6
RosettaHalt <halt.develop@gmail.com>
2013-03-05 03:40:37 +0900

    fix single.
    サイドバーと本カラム追加

--- single.php
+++ single.php
@@ -13,8 +13,24 @@
                 <div class="row">
                     <div class="span9">
                         <h1>怒濤のテストページ</h1>
+                        <div class="well mini-layout-body">
+                            <script type="text/javascript" src="http://jsdo.it/blogparts/xdue/js?width=465&height=496&view=screenshot"></script>
+                        </div>
                     </div>
-                    <div class="span3 well">side</div>
+                    <div class="span3">
+                        <div class="well sidebar-nav">
+                            <ul class="nav nav-list">
+                                <li class="nav-header">nav title</li>
+                                <li>dummy</li>
+                                <li>dummy</li>
+                                <li>dummy</li>
+                                <li class="nav-header">nav title</li>
+                                <li>dummy</li>
+                                <li>dummy</li>
+                                <li>dummy</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
