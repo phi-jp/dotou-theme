3f1232361f91fee58f2737db33d270012b203474
RosettaHalt <halt.develop@gmail.com>
2013-03-05 03:16:45 +0900

    create single

---
+++ single.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <?php get_template_part('head'); ?>
+    </head>
+
+    <body>
+        <?php get_header(); ?>
+
+        <div id="wrap">
+            <div class="container">
+                <div class="row">
+                    <div class="span9">
+                        <h1>怒濤のテストページ</h1>
+                    </div>
+                    <div class="span3 well">side</div>
+                </div>
+            </div>
+        </div>
+
+        <?php get_footer(); ?>
+
+        <?php wp_footer(); ?>
+    </body>
+</html>
