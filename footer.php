c5e4726d15aa5ba117460542a8bc1d2528ab82de
phi <phi1618jp@gmail.com>
2013-03-05 00:22:04 +0900

    fix footer

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
-<footer id="footer">
-    <address>
-        Copyright &copy; <?php bloginfo('name'); ?>. All Rights Reserved.
-    </address>
+<footer id="footer" class="footer">
+	<div class="container">
+	    <address>
+	        Copyright &copy; <?php bloginfo('name'); ?>. All Rights Reserved.
+	    </address>
+		<ul class="footer-links">
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Blog</a></li>
+		</ul>
+	</div>
 </footer>
