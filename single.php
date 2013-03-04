fe3fb1c89727963d7d653f6d47f3c7d0bc3a4e86
RosettaHalt <halt.develop@gmail.com>
2013-03-05 04:51:12 +0900

    fix single.php
    エントリーページのCMS化

--- single.php
+++ single.php
@@ -12,12 +12,42 @@
             <div class="container">
                 <div class="row">
                     <div class="span9">
-                        <h1>怒濤のテストページ</h1>
+                        <?php
+                        if(have_posts()): while(have_posts()):the_post(); ?>
+                        <header>
+                            <h1><?php the_title(); ?></h1>
+                        </header>
+
                         <div class="well">
                             <div class="code-box">
-                                <script type="text/javascript" src="http://jsdo.it/blogparts/xdue/js?width=465&height=496&view=screenshot"></script>
+                            <?php
+                            $jsdoit = get_post_meta( $post->ID, 'jsdoit', true);
+                            $jsdoitThumbnail = get_post_meta( $post->ID, 'jsdoit_thumbnail', true);
+                            $jsdoitType = "screenshot";
+                            if($jsdoitThumbnail === "Auto play"){
+                                $jsdoitType = "play";
+                            }
+                            else if($jsdoitThumbnail === "Screenshot"){
+                                $jsdoitType = "screenshot";
+                            }
+                            else if($jsdoitThumbnail === "Readme"){
+                                $jsdoitType = "";
+                            }
+                            else if($jsdoitThumbnail === "JavaScript"){
+                                $jsdoitType = "javascript";
+                            }
+                            else if($jsdoitThumbnail === "HTML"){
+                                $jsdoitType = "html";
+                            }
+                            else if($jsdoitThumbnail === "CSS"){
+                                $jsdoitType = "css";
+                            }
+                            echo '<script type="text/javascript" src="http://jsdo.it/blogparts/'.$jsdoit.'/js?width=465&height=496&view='.$jsdoitType.'"></script>';
+                            ?>
                             </div>
                         </div>
+                        <?php endwhile; endif; ?>
+
                         <ul class="pager">
                             <li class="previous"><a href="">&larr; prev</a></li>
                             <li class="next"><a href="">next &rarr;</a></li>
