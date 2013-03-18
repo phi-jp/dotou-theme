3da26fce33b66a477cbba0625d561d59731db5af
Halt <halt.develop@gmail.com>
2013-03-18 17:31:08 +0900

    fix archive
    アーカイブのリンクを修正

--- archive-children.php
+++ archive-children.php
@@ -20,7 +20,7 @@
             echo '<h2>'.$value[0]->ChapterName.'</h2>';
             echo '<ol>';
             foreach ($value as $key2 => $value2) {
-                echo '<li><a href="'.$value2->guid.'">'.$value2->post_title.'</a></li>';
+                echo '<li><a href="'.$value2->link.'">'.$value2->post_title.'</a></li>';
             }
             echo '</ol>';
         }
