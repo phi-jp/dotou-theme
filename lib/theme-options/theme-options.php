e85c44cd654e5ad0be988fc06aea0d36268ede42
Halt <halt.develop@gmail.com>
2013-04-13 22:32:08 +0900

    ベースの部分実装

--- lib/theme-options/theme-options.php
+++ lib/theme-options/theme-options.php
@@ -4,6 +4,7 @@ $lang_category = array("_shuren", "_tanren", "_jukuren");
 
 add_action( 'admin_init', 'theme_options_init' );
 add_action( 'admin_menu', 'theme_options_add_page' );
+add_action( 'admin_head', 'theme_head', 100 );
 
 /**
  * Init plugin options to white list our options
@@ -17,6 +18,27 @@ function theme_options_init(){
  */
 function theme_options_add_page() {
     add_theme_page( __( 'Theme Options', 'DOTOU_OPTIONS' ), __( 'Theme Options', 'DOTOU_OPTIONS' ), 'edit_theme_options', 'theme_options', 'theme_options_do_page' );
+}
+
+function theme_head(){
+    ?>
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/lib/bootstrap/admin_css/bootstrap.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.2.1/bootstrap.min.js"></script>
+    <script type="text/javascript">
+    jQuery(function() {
+        // showTab(0);
+        // var tab = jQuery(".nav-tabs li");
+        // tab.click(function(){
+        //     showTab( jQuery(this).index() );
+        // });
+
+        // function showTab(num){
+        //     jQuery(".tab-pane").hide();
+        //     jQuery(".tab-pane").eq(num).show();
+        // }
+    });
+    </script>
+    <?php
 }
 
 /**
@@ -36,36 +58,65 @@ function theme_options_do_page() {
       	<div class="updated fade"><p><strong><?php _e( 'Options saved', 'DOTOU_OPTIONS' ); ?></strong></p></div>
       	<?php endif; ?>
 
-      	<form method="post" action="options.php">
-        		<?php
-            settings_fields( 'dotou_options' );
-        		$options = get_option( 'dotou_theme_options' );
-            $lang_list = getLanguageData($key_name);
-            ?>
+        <form method="post" action="options.php">
 
-        		<table class="form-table">
-                <tr valign="top"><th scope="row">LanguageList</th>
-                    <td>
-                        <textarea id="dotou_theme_options[<?php echo $key_name; ?>]" class="large-text" cols="50" rows="10" name="dotou_theme_options[<?php echo $key_name; ?>]"><?php echo esc_textarea( $options[$key_name] ); ?></textarea>
-                    </td>
-                </tr>
+            <div class="tabbable">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#tab1" data-toggle="tab">Section 1</a></li>
+                    <li><a href="#tab2" data-toggle="tab">Section 2</a></li>
+                </ul>
+
                 <?php
-                foreach ($lang_list as $key => $value) {
-                    $array_key = $key_name."_".trim($value);
-                    echo '<tr valign="top"><th scope="row">'.$value.'</th>';
-                    foreach ($lang_category as $key2 => $value2) {
-                  ?>
-                        <td>
-                            <textarea id="dotou_theme_options[<?php echo $array_key.$value2; ?>]" class="large-text" cols="50" rows="10" name="dotou_theme_options[<?php echo $array_key.$value2; ?>]"><?php echo esc_textarea( $options[$array_key.$value2] ); ?></textarea>
-                        </td>
-                    <?php }
-                    echo '</tr>';
-                } ?>
-        		</table>
-        		<p class="submit">
-        			<input type="submit" class="button-primary" value="<?php _e( 'Save Options', 'DOTOU_OPTIONS' ); ?>" />
-        		</p>
-      	</form>
+                echo esc_textarea( $options[$key_name] );
+                ?>
+
+                <div class="tab-content">
+                    <div class="tab-pane active" id="tab1">
+                        <?php
+                        settings_fields( 'dotou_options' );
+                        $options = get_option( 'dotou_theme_options' );
+                        ?>
+
+                        <table class="form-table">
+                            <thead>
+                                <tr>
+                                    <th scope="row">LanguageList</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr valign="top">
+                                    <td>
+                                        <textarea id="dotou_theme_options[<?php echo $key_name; ?>]" class="large-text" cols="50" rows="10" name="dotou_theme_options[<?php echo $key_name; ?>]"><?php echo esc_textarea( $options[$key_name] ); ?></textarea>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <?php
+                        $lang_list = getLanguageData($key_name);
+                        $tmp = array();
+                        foreach ($lang_list as $key => $value) {
+                            array_push($tmp, $value);
+
+                            if( count($tmp) === 5 ){
+                                showTable($tmp);
+                                $tmp = array();
+                            }
+                        }
+                        if( count($tmp) < 5 ){
+                            showTable($tmp);
+                        }
+                        ?>
+                    </div>
+                    <div class="tab-pane" id="tab2">
+                        <p>Howdy, I'm in Section 2.</p>
+                    </div>
+                </div>
+            </div>
+            <p class="submit">
+                <input type="submit" class="button-primary" value="<?php _e( 'Save Options', 'DOTOU_OPTIONS' ); ?>" />
+            </p>
+        </form>
     </div>
     <?php
 }
@@ -78,13 +129,12 @@ function theme_options_validate( $input ) {
     $lang_list = getLanguageData($key_name);
 
     $input[$key_name] = wp_filter_nohtml_kses( $input[$key_name] );
+    access_logger("key: " . $key_name. " | ". "value: ".$input[$key_name]."\n");
 
     foreach ($lang_list as $key => $value) {
         $array_key = $key_name."_".trim($value);
-        foreach ($lang_category as $key2 => $value2) {
-            $input[$array_key.$value2] = wp_filter_nohtml_kses( $input[$array_key.$value2] );
-            //access_logger("key: " . $array_key.$value2. " | ". "value: ".$input[$array_key.$value2]."\n");
-        }
+        $input[$array_key] = wp_filter_nohtml_kses( $input[$array_key] );
+        access_logger("key: " . $array_key. " | ". "value: ".$input[$array_key]."\n");
     }
 
     return $input;
@@ -98,6 +148,29 @@ function getLanguageData($key_name){
     return $lang_list;
 }
 
+function showTable($tmp){
+    settings_fields( 'dotou_options' );
+    $options = get_option( 'dotou_theme_options' );
+    echo '<table class="form-table">';
+    echo '<thead><tr>';
+    foreach ($tmp as $key => $value) { ?>
+        <th scope="row"><?php echo $value; ?></th>
+    <?php }
+    echo '</tr></thead>';
+    echo '<tbody><tr>';
+    foreach ($tmp as $key => $value) {
+        $form_key = $key_name."_".trim($value);
+        ?>
+        <td scope="row">
+            <textarea id="dotou_theme_options[<?php echo $form_key; ?>]" class="large-text" cols="50" rows="10" name="dotou_theme_options[<?php echo $form_key; ?>]"><?php echo esc_textarea( $options[$form_key] ); ?></textarea>
+        </td>
+    <?php }
+    echo '</tr></tbody>';
+    echo '</table>';
+}
+
 function access_logger($str){
+    $hr = "\n-----------------------------------------------\n\n";
     error_log( $str, 3, "/home/sites/heteml/users/k/i/k/kiki2965/web/study/phi/halt/wp-content/themes/dotou-theme/log/errors.log" );
+    error_log( $hr, 3, "/home/sites/heteml/users/k/i/k/kiki2965/web/study/phi/halt/wp-content/themes/dotou-theme/log/errors.log" );
 }
