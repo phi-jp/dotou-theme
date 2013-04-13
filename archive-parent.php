0dca91bb649cde6df3455f40f0452e10723a3ed0
Halt <halt.develop@gmail.com>
2013-04-13 17:01:55 +0900

    fix archive
    AWSだとリダイレクトできないので修正

--- archive-parent.php
+++ archive-parent.php
@@ -1,6 +1,12 @@
 <?php
 $this_cat = get_category($cat);
 $url = get_bloginfo('home')."/".$this_cat->slug."/".$this_cat->slug."-shuren/";
-header("Location: ". $url);
-exit;
+// header("Location: ". $url);
+// exit;
 ?>
+<script type="text/javascript">
+  function reDirect(){
+    top.location.href="<?php echo $url; ?>";
+  }
+  reDirect();
+</script>
